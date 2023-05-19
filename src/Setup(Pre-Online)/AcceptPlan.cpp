4ae1c4eb5c9bc5a130e7a5a09aa3485a632e0700
Baher <ferventfrost5@gmail.com>
2023-05-20 01:26:06 +0300

    Add AcceptPlan Class

--- src/Setup(Pre-Online)/AcceptPlan.cpp
+++ src/Setup(Pre-Online)/AcceptPlan.cpp
@@ -1,53 +1,105 @@
 #include "AcceptPlan.h"
 
-void AcceptPlan::ListenPlan()
+AcceptData::AcceptData()
 {
-    // This method listen for the plan using Websocket from Ground Station
-    // Send plan is received to Ground Station as a response
-    // Parse plan's header and return it (Plan ID, Plan Length, Plan Checksum)
+    PlanCounter = 0;
+    Plan = new StructPlanBody[PLAN_MAX];
 }
 
-void AcceptPlan::AcceptInvalidPlan()
+// void AcceptData::ListenPlan()
+// {
+//     // This method listen for the plan using Websocket from Ground Station
+//     // Send plan is received to Ground Station as a response
+//     // Parse plan's header and return it (Plan ID, Plan Length, Plan Checksum)
+// }
+
+// void AcceptData::AcceptInvalidPlan()
+// {
+//     // This method accept the invalid plan by sending the plan ID to Ground Station
+//     // Send plan is received to Ground Station as a response
+// }
+
+// void AcceptData::StorePlan()
+// {
+//     // This method store the plan in the EEPROM and return the plan ID and Address
+//     // Send plan is Done to Ground Station as a response
+// }
+
+// void AcceptData::EndAcceptPlan()
+// {
+//     // This method end the Accept Plan process by sending the plan ID to Ground Station
+// }
+
+// bool AcceptData::isPlanValid()
+// {
+//     // This method check if the plan is valid using the checksum
+//     // and return true if the plan is valid
+//     // and return false if the plan is invalid
+
+//     // Send plan is invalid to Ground Station as a PlanResend response
+//     return true;
+// }
+
+// bool AcceptData::isPlanAvailable()
+// {
+//     // This method check Plan Counter and return true if Plan Counter is less than or equal PLAN_MAX
+//     return true;
+// }
+
+// void AcceptData::ExecuteAcceptPlan()
+// {
+//     // 1. Check if plan is available using isPlanAvailable()
+//     // 2. If plan is available, accept the plan using AcceptPlan()
+//     // 3. If plan is not available, go to step 8
+//     // 4. check if plan is valid using isPlanValid()
+//     // 5. If plan is valid, store the plan using StorePlan()
+//     // 6. If plan is invalid, accept the invalid plan using AcceptInvalidPlan() by sending the plan ID to Ground Station and return to step 4
+//     // 7. If plan is valid, return to step 1
+//     // 8. End the Accept Plan process using EndAcceptPlan() by sending the plan ID to Ground Station
+// }
+
+void AcceptData::AcceptHeader(byte *buffer)
 {
-    // This method accept the invalid plan by sending the plan ID to Ground Station
-    // Send plan is received to Ground Station as a response
+    Header = Serialization::DeserializeHeader(buffer);
 }
 
-void AcceptPlan::StorePlan()
+bool AcceptData::AcceptPlan(byte *buffer)
 {
-    // This method store the plan in the EEPROM and return the plan ID and Address
-    // Send plan is Done to Ground Station as a response
-}
 
-void AcceptPlan::EndAcceptPlan()
-{
-    // This method end the Accept Plan process by sending the plan ID to Ground Station
-}
+    if (!Serialization::isValidCRC(buffer, 6, Header.CRC))
+        return false;
 
-bool AcceptPlan::isPlanValid()
-{
-    // This method check if the plan is valid using the checksum
-    // and return true if the plan is valid
-    // and return false if the plan is invalid
+    StructPlanBody plan = Serialization::DeserializePlanBody(buffer);
+    Plan[PlanCounter] = plan;
+    PlanCounter++;
 
-    // Send plan is invalid to Ground Station as a PlanResend response
     return true;
 }
 
-bool AcceptPlan::isPlanAvailable()
+bool AcceptData::AcceptCommand(byte *buffer)
 {
-    // This method check Plan Counter and return true if Plan Counter is less than or equal PLAN_MAX
+    if (!Serialization::isValidCRC(buffer, 7, Header.CRC))
+        return false;
+
+    StructBody command = Serialization::DeserializeBodyOnline(buffer);
+    SaveCommand(command);
+
     return true;
 }
 
-void AcceptPlan::ExecuteAcceptPlan()
+void AcceptData::ResetCommandAdress()
 {
-    // 1. Check if plan is available using isPlanAvailable()
-    // 2. If plan is available, accept the plan using AcceptPlan()
-    // 3. If plan is not available, go to step 8
-    // 4. check if plan is valid using isPlanValid()
-    // 5. If plan is valid, store the plan using StorePlan()
-    // 6. If plan is invalid, accept the invalid plan using AcceptInvalidPlan() by sending the plan ID to Ground Station and return to step 4
-    // 7. If plan is valid, return to step 1
-    // 8. End the Accept Plan process using EndAcceptPlan() by sending the plan ID to Ground Station
+    ESPFlash::SetWriteAddress(0);
+}
+
+// Save the command to the flash memory
+void AcceptData::SaveCommand(StructBody command)
+{
+    ESPFlash::WriteCommand(command);
+}
+
+// Please Delete the returned pointer after use
+StructPlanBody *AcceptData::ReturnPlans()
+{
+    return Plan;
 }
--- src/Setup(Pre-Online)/AcceptPlan.h
+++ src/Setup(Pre-Online)/AcceptPlan.h
@@ -1,25 +1,39 @@
 #ifndef AcceptPlan_H
 #define AcceptPlan_H
 
+#include "StructPlan.h"
+#include "Serialization/Serialization.h"
+#include "Command/ESPFlash.h"
 #include "Arduino.h"
 
-class AcceptPlan
+#define PLAN_MAX 5
+
+class AcceptData
 {
-    private:
-        // Data
-        static const byte PLAN_MAX = 5;
-        byte planCounter;
-        byte planID[5];
-    public:
-        AcceptPlan();
-        void ListenPlan();
-        void StorePlan();
-        void AcceptInvalidPlan();
-        void ExecuteAcceptPlan();
-        void EndAcceptPlan();
-    protected:
-        bool isPlanValid();
-        bool isPlanAvailable();
+private:
+    // Data
+    byte PlanCounter;
+    StructPlanBody *Plan;
+    StructHeader Header;
+
+public:
+    AcceptData();
+    // void ListenPlan();
+    // void StorePlan();
+    // void AcceptInvalidPlan();
+    // void ExecuteAcceptPlan();
+    // void EndAcceptPlan();
+
+    void AcceptHeader(byte *buffer);
+    bool AcceptPlan(byte *buffer);
+    bool AcceptCommand(byte *buffer);
+    void ResetCommandAdress();
+    StructPlanBody *ReturnPlans();
+
+protected:
+    // bool isPlanValid();
+    // bool isPlanAvailable();
+    void SaveCommand(StructBody command);
 };
 
-#endif 
+#endif

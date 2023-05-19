6ecc7f54a33639bfad55ebeef65a5cc56fbe7a8e
Baher <ferventfrost5@gmail.com>
2023-05-20 02:45:16 +0300

    Add telemerty Class

--- src/Setup(Pre-Online)/Telemetery.h
+++ src/Setup(Pre-Online)/Telemetery.h
@@ -1,22 +1,33 @@
 #ifndef Telemetery_H
 #define Telemetery_H
 
+#include "Serialization/Serialization.h"
+#include "Command/SDCard.h"
 #include "Arduino.h"
 
 class Telemetery
 {
-    private:
-        String planAddress;
-        byte plan[];
-    public:
-        Telemetery();
-        ~Telemetery();
-        void RetriveTelemetery();
-        void SendTelemetery();
-        void DeleteTelemetery();
-        bool NextPlan();
-        bool IsPlanAvailable();
-        void RunTelemetery();
-        void SendEndOfTelemetery();
+private:
+    fs::File File;
+    char *Path;
+    String planAddress;
+    byte plan[];
+
+public:
+    Telemetery();
+    ~Telemetery();
+
+    byte *GetTelemetery();
+    void SetPath(char *path);
+    void OpenFile();
+    void CloseFile();
+
+    // void RetriveTelemetery();
+    // void SendTelemetery();
+    // void DeleteTelemetery();
+    // bool NextPlan();
+    // bool IsPlanAvailable();
+    // void RunTelemetery();
+    // void SendEndOfTelemetery();
 };
 #endif
